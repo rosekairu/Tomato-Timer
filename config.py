c985bf9c5ece6542202c4330fe0b301bbe6317ac
Rose Kairu <rosekairu@gmail.com>
2020-06-25 03:57:23 +0300

    modified requirements.txt and config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rose:kairu@localhost/pomodoro'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     UPLOADED_PHOTOS_DEST ='app/static/photos'
